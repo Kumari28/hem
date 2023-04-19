ebc4d06f2c7c7da197deb8459092ce5aea4ebac9
Kumari28 <97704492+Kumari28@users.noreply.github.com>
2023-04-19 20:35:28 +0530

    string class created

---
+++ string.java
@@ -0,0 +1,22 @@
+public class Lab457 {
+
+	public static void main(String[]args) {
+		
+	String str="jlc";
+	
+	String str2="jlc";
+	
+	String str3="jlc";
+	
+	System.out.println(str);
+		
+	System.out.println(str2);
+
+	System.out.println(str3);
+
+	System.out.println(str==str2);
+	
+	System.out.println(str==str3);
+
+	}
+}
